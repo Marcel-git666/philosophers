6b2044db65182ac2e5f9d92ac3cd361a2a979abf
Marcel Mravec <marcel.mravec@gmail.com>
2024-11-16 16:28:20 +0100

    test

--- philo/test.c
+++ philo/test.c
@@ -3,25 +3,35 @@
 #include <pthread.h>
 #include <unistd.h>
 
-void* routine() {
-	printf("Hello from threads\n");
-	sleep(3);
-	printf("Ending thread\n");
+int primes[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+
+void* routine(void *arg) {
+	int index = *(int*) arg;
+	int sum = 0;
+	for (int i = 0, i < 5, i++) {
+		sum += primes[i +  index];
+	}
+	printf("Sum of primes from thread: %d\n", sum);
+	*(int *) arg = sum;
+	return (arg);
 }
 
 int main(int argc, char* argv[]) {
-	pthread_t p1, p2;
-	if (pthread_create(&p1, NULL, &routine, NULL) != 0) {
-		return 1;
+	pthread_t th[2];
+
+	for (int i = 0; i < 2; i++) {
+		int *a = malloc(sizeof(int));
+		*a = i * 5;
+		if (pthread_create(th+i, NULL, &routine, a) != 0) {
+		return (1);
+		}
 	}
-	if (pthread_create(&p2, NULL, &routine, NULL) != 0) {
-		return 2;
+	for (int i = 0; i < 2; i++) {
+		int *r;
+		if (pthread_join(th[i], &r) != 0) {
+				return 2;
+		}
 	}
-	if (pthread_join(p1, NULL) != 0) {
-		return 3;
-	}
-	if (pthread_join(p2, NULL) != 0) {
-		return 4;
-	}
+	
 	return 0;
 }
